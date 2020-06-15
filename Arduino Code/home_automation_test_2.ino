0abd5588e74d7443e9b0a6bcfd59427fff4a23ff
LuCiFeR_ash <45912425+LuCiFeR789@users.noreply.github.com>
2020-06-16 02:06:39 +0530

    Create home_automation_test_2.ino
    Added Arduino Code v1

---
+++ Arduino Code/home_automation_test_2.ino
@@ -0,0 +1,109 @@
+#define RELAY_ON 0
+#define RELAY_OFF 1
+
+#define RELAY_1  8
+#define RELAY_2  9
+#define RELAY_3  10
+#define RELAY_4  11
+char data = 0;
+
+void setup() {
+
+// Set pin as output.
+
+pinMode(RELAY_1, OUTPUT);
+pinMode(RELAY_2, OUTPUT);
+pinMode(RELAY_3, OUTPUT);
+pinMode(RELAY_4, OUTPUT);
+
+// Initialize RELAY1 = off. So that on reset it would be off by default
+
+digitalWrite(RELAY_1, RELAY_OFF);
+digitalWrite(RELAY_2, RELAY_OFF);
+digitalWrite(RELAY_3, RELAY_OFF);
+digitalWrite(RELAY_4, RELAY_OFF);
+Serial.begin(9600);
+
+Serial.print("1 to turn bulb on and 0 to trn it off");
+
+}
+
+void loop() {
+
+ if (Serial.available() > 0) {
+
+data = Serial.read();      //Read the incoming data and store it into variable data
+
+  Serial.print(data);        //Print Value inside data in Serial monitor
+
+Serial.print("\n");        //New line
+
+if(data == '1'){
+
+digitalWrite(RELAY_1, RELAY_ON);
+
+Serial.println("Device 1 is on");
+
+}
+if(data == '2'){
+
+digitalWrite(RELAY_2, RELAY_ON);
+
+Serial.println("Device 2 is on");
+
+}
+if(data == '3'){
+
+digitalWrite(RELAY_3, RELAY_ON);
+
+Serial.println("Device 3 is on");
+
+}
+if(data == '4'){
+
+digitalWrite(RELAY_4, RELAY_ON);
+
+Serial.println("Device 4 is on");
+
+}
+
+else if(data == 'a')
+{
+
+digitalWrite(RELAY_1, RELAY_OFF);
+
+Serial.println("Device 1 is off");
+}
+else if(data == 'b')
+{
+
+digitalWrite(RELAY_2, RELAY_OFF);
+
+Serial.println("Device 2 is off");
+}
+else if(data == 'c')
+{
+
+digitalWrite(RELAY_3, RELAY_OFF);
+
+Serial.println("Device 3 is off");
+}
+else if(data == 'd')
+{
+
+digitalWrite(RELAY_4, RELAY_OFF);
+
+Serial.println("Device 4 is off");
+}
+else if(data == '0')
+{
+digitalWrite(RELAY_1, RELAY_OFF);
+digitalWrite(RELAY_2, RELAY_OFF);  
+digitalWrite(RELAY_3, RELAY_OFF);
+digitalWrite(RELAY_4, RELAY_OFF);
+
+Serial.println("Devices are off");
+}
+}
+
+}
